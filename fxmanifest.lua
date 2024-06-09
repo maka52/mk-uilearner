f71488dd8fa93b6d98353375746dd1c00ed36a11
maka52 <166962001+maka52@users.noreply.github.com>
2024-06-09 13:27:08 +0100

    Made open source

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'MK-Scripts (Maka)'
+title 'UI Learner'
+description 'Use this to learn how to work with UI'
+
+client_script 'main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/listener.js'
+}
---
+++ html/listener.js
@@ -0,0 +1,14 @@
+$(function(){
+    window.onload = function(e) {
+        window.addEventListener("message", (event) => {
+            var item  = event.data
+            if(item !== undefined && item.type === "ui") {
+                if (item.display === true) {
+                    $('#container').show();
+                } else {
+                    $('#container').hide();
+                }
+            }
+        })
+    }
+})
